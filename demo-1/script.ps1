f0a2380be1aa5aae8c354a99a583fdd324d1c7ea
AutoExtractor <crgarcia@live.com.ar>
2021-09-08 16:39:24 +0200

    addung more demos

--- demo-1/script.ps1
+++ demo-1/script.ps1
@@ -1,13 +1,29 @@
-az aks create -g cargar-aks-public-rg -n cargar-aks-cluster1
-az aks get-credentials  -g cargar-aks-public-rg -n cargar-aks-cluster1 --admin
-az aks update -g cargar-aks-public-rg -n cargar-aks-cluster1 --attach-acr crgarakspublicacr
+az aks create -g cargar-aks-public-rg -n cargar-aks-cluster2
+az aks get-credentials  -g cargar-aks-public-rg -n cargar-aks-cluster2 --admin
+az aks update -g cargar-aks-public-rg -n cargar-aks-cluster2 --attach-acr crgarakspublicacr
 
-
+https://kubernetes.io/docs/reference/kubectl/cheatsheet/
 
 kubectl get <object>
 kubectl describe <object>
 kubectl apply <dsc>
+kubectl logs <pod>
+kubectl config view
+kubectl cluster-info
 
 kubectl get nodes
 kubectl get namespaces
 kubectl get pods --all-namespaces
+
+# create the function
+func int
+func new
+func host run #remove creds
+func init --docker-only
+docker build . -t crgarakspublicacr.azurecr.io/function2
+
+# Deploy the app
+
+# Service DNS
+kubectl run curl --image=radial/busyboxplus:curl -i --tty
+nslookup myapp
